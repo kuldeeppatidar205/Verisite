4162870b10a265712ed0c8239f2052d88cd57cd7
kuldeep patidar <kuldeeppatel2052007@gmail.com>
2026-05-15 12:59:54 +0530

    minor updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Chatbot from "@/components/Chatbot";
 import { Analytics } from "@vercel/analytics/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -40,6 +40,7 @@ export default function RootLayout({
       </body>
     </html>
     <Analytics/> 
+    <SpeedInsights/>
     </>
   );
 }
